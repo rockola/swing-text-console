6d80611159b1f34137d937770577dd88f9dee1bb
ooxi <violetland@mail.ru>
2013-01-16 19:28:12 +0100

    Maven development...

---
+++ src/main/java/com/github/ooxi/swingtextconsole/App.java
@@ -0,0 +1,13 @@
+package com.github.ooxi.swingtextconsole;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
